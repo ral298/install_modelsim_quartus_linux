9608d42877ea169d77e1feb6e2c6513e4cca05e7
ral298 <81544684+ral298@users.noreply.github.com>
2024-04-09 10:13:29 -0600

    Add files via upload

---
+++ install_libs_modelsim_altera.sh
@@ -0,0 +1,9 @@
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install build-essential
+    sudo apt install gcc-multilib g++-multilib lib32z1 \
+    lib32stdc++6 libxt6:i386 libxtst6:i386 expat:i386 \
+    fontconfig:i386 libfreetype6:i386 libexpat1:i386 libc6:i386 \
+    libgtk-3-0:i386 libcanberra0:i386 libice6:i386 libsm6:i386 \
+    libncurses5:i386 zlib1g:i386 libx11-6:i386 libxau6:i386 \
+    libxdmcp6:i386 libxext6:i386 libxft2:i386 libxrender1:i386
