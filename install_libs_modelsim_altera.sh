931f43e8dcf9853cd99d79444ef013d4dd4c4259
ral298 <81544684+ral298@users.noreply.github.com>
2024-04-17 18:39:15 -0600

    Update install_libs_modelsim_altera.sh

--- install_libs_modelsim_altera.sh
+++ install_libs_modelsim_altera.sh
@@ -7,3 +7,44 @@ sudo apt install build-essential
     libgtk-3-0:i386 libcanberra0:i386 libice6:i386 libsm6:i386 \
     libncurses5:i386 zlib1g:i386 libx11-6:i386 libxau6:i386 \
     libxdmcp6:i386 libxext6:i386 libxft2:i386 libxrender1:i386
+cd
+
+TEXTO="# USB-Blaster
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6002", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6003", MODE="0666"
+
+# USB-Blaster II
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666""
+
+# Define la ubicación y el nombre del archivo temporal
+UBICACION_TEMPORAL="./92-usbblaster.rules"
+
+# Guarda el texto en el archivo temporal
+echo "$TEXTO" > "$UBICACION_TEMPORAL"
+
+sudo mv "$UBICACION_TEMPORAL" "/etc/udev/rules.d/92-usbblaster.rules"
+
+
+
+
+cd
+
+TEXTO1="# USB Blaster
+SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
+SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6002", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
+SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6003", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
+
+# USB Blaster II
+SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
+SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c""
+
+# Define la ubicación y el nombre del archivo temporal
+UBICACION_TEMPORAL1="./51-usbblaster.rules"
+
+# Guarda el texto en el archivo temporal
+echo "$TEXTO1" > "$UBICACION_TEMPORAL1"
+
+sudo mv "$UBICACION_TEMPORAL1" "/etc/udev/rules.d/51-usbblaster.rules"
