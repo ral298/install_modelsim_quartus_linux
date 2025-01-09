37c3e77ee2bfed48f9cd1cb3da6e7e9c21afe869
ral298 <81544684+ral298@users.noreply.github.com>
2025-01-08 18:06:24 -0600

    Add files via upload

--- install_libs_modelsim_altera.sh
+++ install_libs_modelsim_altera.sh
@@ -1,3 +1,24 @@
+# Define la ubicación y el nombre del archivo temporal
+UBICACION_TEMPORAL="./92-usbblaster.rules"
+
+
+sudo cp "$UBICACION_TEMPORAL" "/etc/udev/rules.d/92-usbblaster.rules"
+
+
+
+
+
+
+# Define la ubicación y el nombre del archivo temporal
+UBICACION_TEMPORAL1="./51-usbblaster.rules"
+
+# Guarda el texto en el archivo temporal
+
+
+sudo cp "$UBICACION_TEMPORAL1" "/etc/udev/rules.d/51-usbblaster.rules"
+
+cd
+
 sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:linuxuprising/libpng12
 sudo apt update
@@ -13,52 +34,20 @@ sudo apt install dos2unix
 sudo apt install make
 sudo apt install libpng12-0
 sudo apt-get build-dep -a i386 libfreetype6
+sudo apt install make
 
-wget https://master.dl.sourceforge.net/project/freetype/freetype2/2.4.12/freetype-2.4.12.tar.bz2?viasf=1
+
+
+wget https://master.dl.sourceforge.net/project/freetype/freetype2/2.4.12/freetype-2.4.12.tar.bz2
+
+
 
 tar -xjvf freetype-2.4.12.tar.bz2
 cd freetype-2.4.12
 ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
 make -j8
 sudo make install
-cd
-
-TEXTO="# USB-Blaster
-SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE="0666"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6002", MODE="0666"
-
-SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6003", MODE="0666"
-
-# USB-Blaster II
-SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666""
-
-# Define la ubicación y el nombre del archivo temporal
-UBICACION_TEMPORAL="./92-usbblaster.rules"
-
-# Guarda el texto en el archivo temporal
-echo "$TEXTO" > "$UBICACION_TEMPORAL"
-
-sudo mv "$UBICACION_TEMPORAL" "/etc/udev/rules.d/92-usbblaster.rules"
 
 
 
 
-cd
-
-TEXTO1="# USB Blaster
-SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
-SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6002", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
-SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6003", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
-
-# USB Blaster II
-SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c"
-SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666", NAME="bus/usb/$env{BUSNUM}/$env{DEVNUM}", RUN+="/bin/chmod 0666 %c""
-
-# Define la ubicación y el nombre del archivo temporal
-UBICACION_TEMPORAL1="./51-usbblaster.rules"
-
-# Guarda el texto en el archivo temporal
-echo "$TEXTO1" > "$UBICACION_TEMPORAL1"
-
-sudo mv "$UBICACION_TEMPORAL1" "/etc/udev/rules.d/51-usbblaster.rules"
